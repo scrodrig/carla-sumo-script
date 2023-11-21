a867b19741d53cd6ba7a3ed4a668e68044e10343
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2023-11-21 18:36:56 +0000

    adds pandas and open files

--- plot-generator.py
+++ plot-generator.py
@@ -2,18 +2,28 @@ import argparse
 from util.reader import load_dataset
 from loguru import logger
 
+"""
+Entry point to the plot generator
+"""
+
 
 def main():
     logger.critical("Carla-Sumo - Script plot generation")
-    path = load_dataset(arguments)
+    all_dataset = load_dataset(arguments.csv_file)
+    nearby_dataset = load_dataset(arguments.nearby_csv_file, event_file=False)
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument(
-        "--path-carla",
+        "--csv-file",
         type=str,
-        help="path to carla file",
+        help="all events csv file",
+    )
+    argparser.add_argument(
+        "--nearby-csv-file",
+        type=str,
+        help="nearby csv file for all events",
     )
     arguments = argparser.parse_args()
 
--- util/reader.py
+++ util/reader.py
@@ -1,7 +1,62 @@
+"""
+Class to load csv into datasets
+"""
 from loguru import logger
+from pathlib import Path
+import pandas as pd
 
 
-def load_dataset(args):
-    path_carla = args.path_carla
-    logger.info('{}: {}'.format("PATH TO FILE", path_carla))
-    return 0
+cols_all = [
+    "CarlaId",
+    "SumoId",
+    "Model",
+    "Role",
+    "Carla X",
+    "Carla Y",
+    "Carla Z",
+    "SUMO X",
+    "SUMO Y",
+    "SUMO Z",
+    "time",
+]
+
+cols_nearby = [
+    "CarlaId",
+    "Name",
+    "Model",
+    "Role",
+    "Distance",
+    "Carla X",
+    "Carla Y",
+    "Carla Z",
+    "time",
+]
+
+
+"""
+load_dataset opens a csv and returns a DataSet
+"""
+
+
+def load_dataset(csv_name, event_file=True):
+    # This second parent is because we are inside `util`
+    HERE = Path(__file__).parent.parent
+    INPUT_FOLDER = HERE / "input"
+    logger.info("{}: {}".format("PATH TO FILE", INPUT_FOLDER / csv_name))
+
+    colums = cols_all if event_file else cols_nearby
+
+    dataframe_read = pd.read_csv(
+        INPUT_FOLDER / csv_name,
+        encoding='unicode_escape',
+        converters={"Model": str.lower, "Role": str.lower},
+        usecols=colums,
+    )
+
+    logger.info(
+        "{} OF {}: {}".format(
+            "NUMBER OF ROWS", csv_name, dataframe_read.shape[0]
+        )
+    )
+
+    return dataframe_read
